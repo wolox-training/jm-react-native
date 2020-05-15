f2ad872aced8126d9d0a9b2527a8db5e0bf5de69
Juan Camilo Morales <camov1209@gmail.com>
2020-05-15 13:56:23 -0500

    add aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@app': './src/app',
+          '@assets': './src/assets',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@propTypes': './src/propTypes',
+          '@redux': './src/redux',
+          '@scss': './src/scss',
+          '@services': './src/services',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
