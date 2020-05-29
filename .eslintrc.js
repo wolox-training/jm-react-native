c8b8c4080141e5e94679243c9acc003f72c8d7b7
Juan Camilo Morales <camov1209@gmail.com>
2020-05-29 16:17:42 -0500

    add reactotron initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'no-magic-numbers': 'off',
     'new-cap': 'off',
     'comma-dangle': 'off',
-    'import/order': ['error', { 'newlines-between': 'always' }]
+    'import/order': ['error', { 'newlines-between': 'always' }],
   },
   settings: {
     'import/ignore': ['node_modules'],
