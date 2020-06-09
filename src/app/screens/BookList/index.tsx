6b7161b14bbc7a7305e499efc3e9ff91d565b4eb
Juan Camilo Morales <camov1209@gmail.com>
2020-06-09 14:03:15 -0500

    remove unused code

--- src/app/screens/BookList/index.tsx
+++ src/app/screens/BookList/index.tsx
@@ -3,7 +3,6 @@ import { FlatList, ListRenderItem } from 'react-native';
 
 import { BOOKS } from '@constants/mockData';
 import { Book } from '@interfaces/book';
-import BookService from '@services/BookService';
 
 import BookCard from './components/BookCard';
 import styles from './styles';
@@ -11,7 +10,6 @@ import styles from './styles';
 function BookList() {
   const keyExtractor = ({ id }: Book) => String(id);
   const renderBook: ListRenderItem<Book> = ({ item }) => <BookCard {...item} />;
-  BookService.getBookDetail(1);
   return (
     <FlatList<Book>
       data={BOOKS}
